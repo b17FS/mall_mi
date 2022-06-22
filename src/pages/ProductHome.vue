78daa66ba3403bfb6db19c2fa322016cc6209917
gitee210918 <3463287725@qq.com>
2022-06-22 13:18:11 +0800

    create project

---
+++ src/pages/ProductHome.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <!-- 头部导航栏nav-header -->
+    <!-- 主体部分 -->
+    首页
+    <router-view></router-view>
+    <!-- 底部导航栏nav-ooter -->
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style></style>
